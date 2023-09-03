c2fbeba52cdbc9e4f8a31b9828717dc742d34a3a
guipmdev <guipmdev@users.noreply.github.com>
2023-09-03 20:39:09 +0000

    style: Update design tokens

--- build/js/tokens.js
+++ build/js/tokens.js
@@ -1,13 +1,17 @@
 /**
  * Do not edit directly
- * Generated on Sun, 03 Sep 2023 20:05:12 GMT
+ * Generated on Sun, 03 Sep 2023 20:39:08 GMT
  */
 
 module.exports = {
+  "Sizes2": 8,
+  "Sizes4": 16,
+  "Sizes8": 32,
   "ColorWhite": "#ffffff",
   "ColorGray200": "#c1c1c4",
   "ColorGray500": "#50505c",
   "ColorGray800": "#1c1c1f",
   "ColorGray900": "#121214",
+  "ColorPurple400": "#9b74f3",
   "ColorPurple500": "#8b5cf6"
 };
