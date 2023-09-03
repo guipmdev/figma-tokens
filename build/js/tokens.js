d40e4a4c78c253d2cb2b9865fe50d5bc6680b628
Guilherme Preveda Milék <guipm.dev@gmail.com>
2023-09-03 17:15:13 -0300

    Initial commit

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Sun, 03 Sep 2023 20:05:12 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray500": "#50505c",
+  "ColorGray800": "#1c1c1f",
+  "ColorGray900": "#121214",
+  "ColorPurple500": "#8b5cf6"
+};
